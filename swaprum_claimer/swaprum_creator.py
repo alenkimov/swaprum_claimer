9572d4305e199a118ee729cefa30735ac0a45aa3
Alen <alen.kimov@gmail.com>
2023-05-15 19:34:41 +0600

    feat: асинхронное создание аккаунтов

--- swaprum_claimer/swaprum_creator.py
+++ swaprum_claimer/swaprum_creator.py
@@ -10,19 +10,25 @@ from swaprum_claimer._web3 import w3
 OUTPUT_DIR.mkdir(exist_ok=True)
 ACCOUNTS_TXT = OUTPUT_DIR / 'accounts.txt'
 
-ACCOUNTS_TO_CREATE = 10
+ACCOUNTS_TO_CREATE = 1000
+
+
+async def _create_and_write(session, file):
+    await create_account(session)
+    account, code = await create_account(session)
+    account_data = ":".join((account.address, w3.to_hex(account.key), f"/active {code}"))
+    file.write(account_data)
+    file.write("\n")
+    print(f"{account_data}")
 
 
 async def main():
     with open(ACCOUNTS_TXT, "w") as file:
         async with aiohttp.ClientSession() as session:
+            tasks = []
             for i in range(ACCOUNTS_TO_CREATE):
-                account, code = await create_account(session)
-                account_data = ":".join((account.address, w3.to_hex(account.key), f"/active {code}"))
-                file.write(account_data)
-                file.write("\n")
-                print(f"{i}: {account_data}")
-
+                tasks.append(asyncio.create_task(_create_and_write(session, file)))
+            await asyncio.gather(*tasks)
 
 if __name__ == '__main__':
     asyncio.run(main())
